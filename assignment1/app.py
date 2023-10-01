c09eee5c7e3f4ef4e37265ad84a080ee40d75ec7
SalmaTADLAOUI <76519142+SalmaTADLAOUI@users.noreply.github.com>
2023-10-02 00:02:06 +0100

    Add files via upload

---
+++ assignment1/app.py
@@ -0,0 +1,37 @@
+import pickle
+
+import numpy as np
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+with open('C:/Users/Salma/Desktop/S3_master/cloud_native_ai_kelloubi/assignment1/svc.pkl', 'rb') as model_file:
+    model = pickle.load(model_file)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+
+    '''
+    pour l'affichage sur html
+    '''
+
+    features = request.form.to_dict()
+    features = list(features.values())
+    features = list(map(float, features))
+    print(features)
+    final_features = np.array(features).reshape(1,6)
+    prediction = model.predict(final_features)
+
+    #select = request.form.get('category')
+    dic = {1: 'Malignant', 0: 'Benign'}
+    output = dic[prediction[0]]
+
+    return render_template('index.html', prediction_text='Breast Cancer prediction is :  {}'.format(output))
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
